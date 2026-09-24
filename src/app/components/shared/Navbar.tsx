459595e64de772fe237130b22f2339ac010b3f62
ijannathalima-ux <ijannathalima@gmail.com>
2026-09-24 20:18:07 +0600

    Implement navbar layout and navigation

---
+++ src/app/components/shared/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Logo from "@/app/assets/logo.png";
+import Link from "next/link";
+
+const NavbarPage = () => {
+    return (
+        <div className="bg-[#0C0D10]">
+        <div className="container mx-auto p-6">
+            <div className="flex justify-between">
+
+                {/* Navbar logo */}
+                <div className="flex justify-center items-center gap-2">
+                    <Image src={Logo} alt="FitLog logo" />
+                    <span className="text-[#FFFFFF] font-bold text-[20px]">FITLOG</span>
+                </div>
+
+                <ul className="flex justify-center items-center gap-4 text-[19px] text-[#9CA3AF] font-medium">
+                    <li><Link href="/">Workouts</Link></li>
+                    <li><Link href="/my-plan">My Plan</Link></li>
+                </ul>
+
+                
+                <div className="flex justify-center items-center gap-9 text-[19px] text-[#9CA3AF] font-medium">
+                    {/* plan button */}
+                    <Link href="/my-plan" className="flex items-center gap-2">
+                    <span> Plan </span>
+                    <span className="bg-[#C2F800] rounded-full py-1 px-3 text-black">0</span>
+                    </Link>
+
+                    {/* saved button */}
+                    <Link href ="/my-plan" className="flex items-center gap-2">
+                    <span> Saved </span>
+                    <span className="border border-[#2D313B] rounded-full text-[#D1D5DB] py-1 px-3">0</span>
+                    </Link>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default NavbarPage;
